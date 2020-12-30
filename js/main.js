b0a2e3e85fccb72984d30cf50e4a33ffb7846f7f
devenock <enockomondi305@gmail.com>
2020-12-30 10:10:38 +0300

    working on the mobile view section

--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ function hideNavbar(){
         navbar.style.display = 'none'
     }
 }
+
+// scroll effect
+//Get the button:
+let myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
